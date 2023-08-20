c827725bc76e5af5eefb6b8bab7d1ab9ac82f195
Afsana Abida <afsanamist37@gmail.com>
2023-08-20 11:25:27 +0200

    flip button created in app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const optionContainer = document.querySelector('.option-container')
+const flipButton = document.querySelector('#flip-button')
+
+//Option Choosing
+let angle = 0
+function flip(){
+const optionShips = Array.from(optionContainer.children)
+    angle = angle === 0 ? 90 : 0
+    optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}deg)`)
+
+}
+
+flipButton.addEventListener('click', flip)
